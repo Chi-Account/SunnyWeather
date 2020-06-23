613183a91f6f6d477dda150eab06b482eaf19a95
张智琦 <chi_account@163.com>
2020-06-23 13:34:01 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,29 @@
+package com.sunnyweather.android.logic
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+
+object Repository {
+
+    /**
+     * liveData() 函数可以自动构建并返回一个 LiveData 对象
+     * 它会为函数类型参数提供一个 suspend 函数的上下文，
+     * 这样我们就可以调用 suspend 函数了。
+     */
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        } catch (e: Exception) {
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+}
